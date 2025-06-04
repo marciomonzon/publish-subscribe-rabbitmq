5c4be1a1066ee4dddb4c86c19581b148b8b56191
marciomonzon <marcio.pcmonzon@gmail.com>
2025-06-03 22:11:57 -0300

    notification api created and tested

--- PublishSubscribeRabbitMq/NotificationsApi/Program.cs
+++ PublishSubscribeRabbitMq/NotificationsApi/Program.cs
@@ -1,6 +1,9 @@
+using NotificationsApi.Subscribers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHostedService<NotifyCustomerCreatedSubscriber>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ PublishSubscribeRabbitMq/NotificationsApi/Subscribers/NotifyCustomerCreatedSubscriber.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using Shared.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace NotificationsApi.Subscribers
+{
+    public class NotifyCustomerCreatedSubscriber : BackgroundService
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private const string Queue = "notifications-service/customer-created";
+        public NotifyCustomerCreatedSubscriber()
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            _connection = connectionFactory.CreateConnection("notifications-service-customer-created-consumer");
+
+            _channel = _connection.CreateModel();
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+
+            consumer.Received += async (sender, eventArgs) =>
+            {
+                var contentArray = eventArgs.Body.ToArray();
+                var contentString = Encoding.UTF8.GetString(contentArray);
+                var message = JsonSerializer.Deserialize<CustomerCreatedEvent>(contentString);
+
+                Console.WriteLine($"Message CustomerCreatedEvent received with Email {message.Email}");
+
+                _channel.BasicAck(eventArgs.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume(Queue, false, consumer);
+
+            return Task.CompletedTask;
+        }
+    }
+}
